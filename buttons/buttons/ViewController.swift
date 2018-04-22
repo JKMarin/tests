1325755c8ffd3519a2e9d9893d77211a269560f2
JKMarin <marin.stcruz@gmail.com>
2018-04-21 22:31:57 -0700

    commit inicial

---
+++ buttons/buttons/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  buttons
+//
+//  Created by user139236 on 4/21/18.
+//  Copyright © 2018 user139236. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if sender.title(for: .normal) == "X" {
+            sender.setTitle("A very long title for this button", for: .normal)
+        } else {
+            sender.setTitle("X", for: .normal)
+        }
+        
+    }
+    
+}
+
